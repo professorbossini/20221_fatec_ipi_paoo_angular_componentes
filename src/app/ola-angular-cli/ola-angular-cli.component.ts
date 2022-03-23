58088df3e17dcdc3085c7616dc4e933571cd4ac1
Rodrigo Bossini <professorbossini@gmail.com>
2022-03-23 18:21:47 -0300

    Estamos na apostila 04, sobre Angular. Instalamos o Bootstrap e testamos com Tabelas. Retomar fazendo Data Binding

---
+++ src/app/ola-angular-cli/ola-angular-cli.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ola-angular-cli',
+  templateUrl: './ola-angular-cli.component.html',
+  styleUrls: ['./ola-angular-cli.component.css']
+})
+export class OlaAngularCliComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
