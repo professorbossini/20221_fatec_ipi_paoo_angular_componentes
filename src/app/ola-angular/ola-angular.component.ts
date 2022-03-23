74ece061541ab14c6e3f55d16dd0a0f244145bdc
Rodrigo Bossini <professorbossini@gmail.com>
2022-03-23 17:12:44 -0300

    Criamos um componente Angular sem usar o CLI. Ajustamos o script start para que o navegador tambem seja aberto

---
+++ src/app/ola-angular/ola-angular.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'ola-angular',
+    templateUrl: './ola-angular.component.html',
+    styleUrls: ['./ola-angular.component.css']
+})
+export class OlaAngularComponent{
+
+    framework = 'React'
+
+}
